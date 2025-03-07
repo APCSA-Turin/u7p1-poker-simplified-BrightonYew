4e77a1f95df2ac8ae60f4e22dce0ba00f45e42b6
BrightonYew <brightony@nycstudents.net>
2025-03-07 00:03:38 +0000

    final

--- src/main/java/com/example/project/Game.java
+++ src/main/java/com/example/project/Game.java
@@ -2,59 +2,78 @@ package com.example.project;
 import java.util.ArrayList;
 
 
+
+
 public class Game{
     public static String determineWinner(Player p1, Player p2,String p1Hand, String p2Hand,ArrayList<Card> communityCards){
-        p2.sortAllCards();
+        p2.sortAllCards(); //sort the cards in case looking for high card
         p1.sortAllCards();
+
+
+        Card highestp1Card; //used to determine highest card in each players hand
+        Card highestp2Card;
+
+
+        if (Utility.getRankValue(p1.getHand().get(0).getRank()) > Utility.getRankValue(p1.getHand().get(1).getRank())) {
+            highestp1Card = p1.getHand().get(0);
+        } else {
+            highestp1Card = p1.getHand().get(1);
+        }
+
+
+        if (Utility.getRankValue(p2.getHand().get(0).getRank()) > Utility.getRankValue(p2.getHand().get(1).getRank())) {
+            highestp2Card = p2.getHand().get(0);
+        } else {
+            highestp2Card = p2.getHand().get(1);
+        }
+
+
         int p1rank = 0;
         int p2rank = 0;
 
-        p1rank = Utility.getHandRanking(p1.playHand(communityCards));
+
+        p1rank = Utility.getHandRanking(p1.playHand(communityCards)); //used to determine whose combo is better
         p2rank = Utility.getHandRanking(p2.playHand(communityCards));
 
-        System.out.println(p1rank + " " + p2rank);
+
+
 
         if (p1rank == p2rank) { //if its the same combo, check whose card is higher
-            System.out.println(p1rank);
             if (Utility.getRankValue(p1.getAllCards().get(4).getRank()) > Utility.getRankValue(p2.getAllCards().get(4).getRank())) {
-                System.out.println("BBBBB");
                 return "Player 1 wins!";
             }
             if (Utility.getRankValue(p1.getAllCards().get(4).getRank()) < Utility.getRankValue(p2.getAllCards().get(4).getRank())) {
-                System.out.println("GGGGGG");
                 return "Player 2 wins!";
             }
-            if (p1rank == 1) { //in cases such as highcard, look for the higher card in your hand
-                System.out.println("test");
-                if (Utility.getRankValue(p1.getHand().get(1).getRank()) > Utility.getRankValue(p2.getHand().get(1).getRank())) {
-                    System.out.println("AAAA");
+            if (p1rank == 1 || p1rank == 5) { //in cases such as highcard or three of a kind, look for the higher card in your hand
+                if (Utility.getRankValue(highestp1Card.getRank()) > Utility.getRankValue(highestp2Card.getRank())) {
                     return "Player 1 wins!";
                 }
-                if (Utility.getRankValue(p1.getHand().get(1).getRank()) < Utility.getRankValue(p2.getHand().get(1).getRank())) {
-                    System.out.println("HHHHHHHH");
+                if (Utility.getRankValue(highestp1Card.getRank()) < Utility.getRankValue(highestp2Card.getRank())) {
                     return "Player 2 wins!";
                 }
             }
-            System.out.println("GGGGGGGGG");
             return "Tie!";
         }
 
-        if (p1rank > p2rank) {
-            System.out.println("EEEEEE");
+
+        if (p1rank > p2rank) { //who has the better combo wins
             return "Player 1 wins!";
         }
         if (p2rank > p1rank) {
-            System.out.println("FFFFFFF");
             return "Player 2 wins!";
         }
+
 
         return "Error";
     }
 
+
     public static void play(){ //simulate card playing
-    
+   
     }
-        
-        
+       
+       
+
 
 }
--- src/main/java/com/example/project/testrunner.java
+++ src/main/java/com/example/project/testrunner.java
@@ -8,17 +8,17 @@ public class testrunner {
         Player player1 = new Player();
         Player player2 = new Player();
         
-        player1.addCard(new Card("7", "♠"));
-        player1.addCard(new Card("7", "♣"));
+        player1.addCard(new Card("A", "♠"));
+        player1.addCard(new Card("A", "♣"));
         
-        player2.addCard(new Card("8", "♠"));
-        player2.addCard(new Card("3", "♣"));
+        player2.addCard(new Card("K", "♠"));
+        player2.addCard(new Card("K", "♣"));
         
         ArrayList<Card> communityCards = new ArrayList<>();
         communityCards.add(new Card("A", "♦"));
-        communityCards.add(new Card("A", "♥"));
-        communityCards.add(new Card("A", "♠"));
-    
+        communityCards.add(new Card("K", "♥"));
+        communityCards.add(new Card("9", "♠"));
+        
         String p1Result = player1.playHand(communityCards);
         String p2Result = player2.playHand(communityCards);
         
